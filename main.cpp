8c1a1a3c563f6d1130bf892e4d2b5d5ccaec5843
zulfawu_hamza <hzulfawu32@gmail.com>
2018-08-29 19:53:04 -0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+#include <conio.h>
+int main()
+{
+ int a;
+   int b;
+    a=2;
+ cout << "enter a number: ";
+ cin >> b;
+    if(b%a==0)
+ {
+     }
+     if(b%a==1){
+        cout<< b <<"is a prime number";}
+     else{
+        if(b%a==0)
+        cout<< b <<"is not a prime number ";}
+
+
+
+
+
+
+
+    return 0;
+}
